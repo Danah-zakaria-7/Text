bd730fceb9ba77fefd1717eeed001b07acf08b9e
Danah zakaria <107366146+Danah-zakaria-7@users.noreply.github.com>
2022-06-13 00:21:55 +0300

    Add files via upload

---
+++ Text.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.text;
+
+/**
+ *
+ * @author user
+ */
+public class Text {
+    
+    
+
+    public static void main(String[] args) {
+       String site ="Hello World!";
+       for(int i=0 ; i<site.length();i++)
+       {
+        char X =site.charAt(i);
+        System.out.println(X);
+       }
+         
+    }
+}
